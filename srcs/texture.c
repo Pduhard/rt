099b4ffb09cb697990eb9bf28dd6abaf007fee31
user42 <aplat712@gmail.com>
2020-05-15 21:18:36 +0200

    Norme plane to triangle

--- srcs/texture.c
+++ srcs/texture.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   texture.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aplat <aplat@student.le-101.fr>            +#+  +:+       +#+        */
+/*   By: aplat <aplat@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/27 02:04:16 by pduhard-          #+#    #+#             */
-/*   Updated: 2020/03/12 18:34:48 by pduhard-         ###   ########lyon.fr   */
+/*   Created: 2020/05/15 21:00:57 by aplat             #+#    #+#             */
+/*   Updated: 2020/05/15 21:10:13 by aplat            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,18 @@ t_4vecf	get_uni_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 
 	text = (t_text_proc *)obj->text.text_param;
 	return (text->color[0]);
-	(void)inter_point; // warning
-	(void)normal_inter; // warning
+	(void)inter_point;
+	(void)normal_inter;
 }
 
 t_4vecf	get_perlin_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 {
-	double	perlin_f;
+	double		perlin_f;
 	t_text_proc	*text;
 	t_4vecf		color;
 
 	text = (t_text_proc *)obj->text.text_param;
-  perlin_f = fabs(compute_3dperlin_factor(inter_point, obj->text.scale.val[0],
+	perlin_f = fabs(compute_3dperlin_factor(inter_point, obj->text.scale.val[0],
 		obj->data->permutation, obj->data->gradient));
 	color.val[0] = text->color[0].val[0] * perlin_f;
 	color.val[1] = text->color[0].val[1] * perlin_f;
@@ -50,7 +50,8 @@ double	compute_wood_factor(t_3vecf inter_point, double scale,
 	double	perlin_f;
 	double	wood_f;
 
-	perlin_f = compute_3dperlin_factor(inter_point, scale, permutation, gradient);
+	perlin_f = compute_3dperlin_factor(inter_point,
+		scale, permutation, gradient);
 	wood_f = (1. + sin((perlin_f / 2.) * 150.)) / 2.;
 	return (wood_f);
 }
@@ -65,23 +66,25 @@ double	marble_noise(t_3vecf inter_point, double scale,
 	{
 		t += fabs(compute_3dperlin_factor(inter_point, scale,
 			permutation, gradient) / scale);
-	  scale *= 2;
+		scale *= 2;
 	}
-	return t;
+	return (t);
 }
 
 double	compute_marble_factor(t_3vecf inter_point, double scale,
 	const unsigned char permutation[512], const t_3vecf gradient[16])
 {
-	double t = (0.5 + 0.5 * sin(scale * 2 * M_PI * (inter_point.val[0] + 2
+	double t;
+
+	t = (0.5 + 0.5 * sin(scale * 2 * M_PI * (inter_point.val[0] + 2
 		* marble_noise(inter_point, scale, permutation, gradient))));
 	return (t * t - .5);
 }
 
 t_4vecf	get_wood_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 {
-	double	wood_f;
-	t_4vecf	color;
+	double		wood_f;
+	t_4vecf		color;
 	t_text_proc	*text;
 
 	text = (t_text_proc *)obj->text.text_param;
@@ -101,8 +104,8 @@ t_4vecf	get_wood_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 
 t_4vecf	get_marble_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 {
-	double	marble_f;
-	t_4vecf	color;
+	double		marble_f;
+	t_4vecf		color;
 	t_text_proc	*text;
 
 	text = (t_text_proc *)obj->text.text_param;
@@ -122,8 +125,8 @@ t_4vecf	get_marble_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 
 t_4vecf	get_fbm_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 {
-	double	fbm_f;
-	t_4vecf	color;
+	double		fbm_f;
+	t_4vecf		color;
 	t_text_proc	*text;
 
 	text = (t_text_proc *)obj->text.text_param;
@@ -140,7 +143,7 @@ t_4vecf	get_fbm_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 	(void)normal_inter;
 }
 
-int     get_pixel_addr(t_2vecf text_coord, t_text_img *text, t_obj *obj)
+int		get_pixel_addr(t_2vecf text_coord, t_text_img *text, t_obj *obj)
 {
 	int		row;
 	int		col;
@@ -166,10 +169,10 @@ int     get_pixel_addr(t_2vecf text_coord, t_text_img *text, t_obj *obj)
 
 t_4vecf	get_image_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 {
-	t_2vecf			text_coord;
-	t_4vecf			color;
+	t_2vecf		text_coord;
+	t_4vecf		color;
 	t_text_img	*text;
-	int					pixel_addr;
+	int			pixel_addr;
 
 	text = (t_text_img *)obj->text.text_param;
 	text_coord = obj->get_text_coordinate(inter_point, normal_inter, obj);
@@ -186,19 +189,19 @@ t_4vecf	get_image_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 
 t_4vecf	get_grid_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 {
-	t_2vecf	text_coord;
+	t_2vecf		text_coord;
 	t_text_proc	*text;
-	int		u_grid;
-	int		v_grid;
+	int			u_grid;
+	int			v_grid;
 
 	text = (t_text_proc *)obj->text.text_param;
 	text_coord = obj->get_text_coordinate(inter_point, normal_inter, obj);
 	text_coord.val[0] *= obj->text.scale.val[0];
 	text_coord.val[1] *= obj->text.scale.val[1];
-	text_coord.val[0] -= text_coord.val[0] < 0 ? (1 + fmod(text_coord.val[0], 1))
-		: fmod(text_coord.val[0], 1);
-	text_coord.val[1] -= text_coord.val[1] < 0 ? (1 + fmod(text_coord.val[1], 1))
-		: fmod(text_coord.val[1], 1);
+	text_coord.val[0] -= text_coord.val[0] < 0 ?
+		(1 + fmod(text_coord.val[0], 1)) : fmod(text_coord.val[0], 1);
+	text_coord.val[1] -= text_coord.val[1] < 0 ?
+		(1 + fmod(text_coord.val[1], 1)) : fmod(text_coord.val[1], 1);
 	u_grid = (fmod(text_coord.val[0], 2) == 0);
 	v_grid = (fmod(text_coord.val[1], 2) == 0);
 	if ((u_grid ^ v_grid))
@@ -206,22 +209,22 @@ t_4vecf	get_grid_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 	return (text->color[1]);
 }
 
-t_4vecf			(*assign_text_color_function(t_text_type type))(t_3vecf,
+t_4vecf	(*assign_text_color_function(t_text_type type))(t_3vecf,
 	t_3vecf, struct s_obj *)
 {
-		if (type == TEXT_PERLIN)
-			return (&get_perlin_color);
-		if (type == TEXT_MARBLE)
-			return (&get_marble_color);
-		if (type == TEXT_WOOD)
-			return (&get_wood_color);
-		if (type == TEXT_FBM)
-			return (&get_fbm_color);
-		if (type == TEXT_UNI)
-			return (&get_uni_color);
-		if (type == TEXT_GRID)
-			return (&get_grid_color);
+	if (type == TEXT_PERLIN)
+		return (&get_perlin_color);
+	if (type == TEXT_MARBLE)
+		return (&get_marble_color);
+	if (type == TEXT_WOOD)
+		return (&get_wood_color);
+	if (type == TEXT_FBM)
+		return (&get_fbm_color);
+	if (type == TEXT_UNI)
 		return (&get_uni_color);
+	if (type == TEXT_GRID)
+		return (&get_grid_color);
+	return (&get_uni_color);
 }
 
 t_text	generate_random_texture(t_obj *obj)
