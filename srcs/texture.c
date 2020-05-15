e319ad40b079dee01a79b575d3ad9883d90e4675
Pduhard <pacodj@free.fr>
2020-05-15 18:14:13 +0200

    norme done except render.c

--- srcs/texture.c
+++ srcs/texture.c
@@ -12,7 +12,6 @@
 
 #include "rt.h"
 
-
 double	linear_interpolate(double a, double b, double val)
 {
 	return (a + (b - a) * val);
@@ -32,24 +31,11 @@ t_4vecf	get_perlin_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 {
 	double	perlin_f;
 	t_text_proc	*text;
-	t_4vecf	color;
+	t_4vecf		color;
+
 	text = (t_text_proc *)obj->text.text_param;
-
-	// obj->data->new_obj = 1;
-	// inter_point.val[2] += obj->data->f;
-
-  perlin_f = fabs(compute_3dperlin_factor(inter_point, obj->text.scale.val[0], obj->data->permutation, obj->data->gradient));
-/*	if (perlin_f > 0.8)
-		return (text->color[0]);
-	//	return (assign_3vecf(obj->text.color[0].val[0] * perlin_f, obj->text.color[0].val[1] * perlin_f, obj->text.color[0].val[2] * perlin_f));
-	else if (perlin_f > 0.5)
-		return (text->color[1]);
-	//	return (assign_3vecf(obj->text.color[1].val[0] * perlin_f, obj->text.color[1].val[1] * perlin_f, obj->text.color[1].val[2] * perlin_f));
-	else
-		return (text->color[2]);
-	//	return (assign_3vecf(obj->text.color_3.val[0] * perlin_f, obj->text.color_3.val[1] * perlin_f, obj->text.color_3.val[2] * perlin_f));
-*/
-//	printf("%f\n", perlin_f);
+  perlin_f = fabs(compute_3dperlin_factor(inter_point, obj->text.scale.val[0],
+		obj->data->permutation, obj->data->gradient));
 	color.val[0] = text->color[0].val[0] * perlin_f;
 	color.val[1] = text->color[0].val[1] * perlin_f;
 	color.val[2] = text->color[0].val[2] * perlin_f;
@@ -58,33 +44,37 @@ t_4vecf	get_perlin_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 	(void)normal_inter;
 }
 
-double	compute_wood_factor(t_3vecf inter_point, double scale, const unsigned char permutation[512], const t_3vecf gradient[16])
+double	compute_wood_factor(t_3vecf inter_point, double scale,
+	const unsigned char permutation[512], const t_3vecf gradient[16])
 {
 	double	perlin_f;
 	double	wood_f;
 
 	perlin_f = compute_3dperlin_factor(inter_point, scale, permutation, gradient);
-	wood_f = (1. + sin((/*text_coord.val[1] + */perlin_f / 2.) * 150.)) / 2.;
-
+	wood_f = (1. + sin((perlin_f / 2.) * 150.)) / 2.;
 	return (wood_f);
 }
 
-double	marble_noise(t_3vecf inter_point, double scale, const unsigned char permutation[512], const t_3vecf gradient[16])
+double	marble_noise(t_3vecf inter_point, double scale,
+	const unsigned char permutation[512], const t_3vecf gradient[16])
 {
 	double	t;
 
 	t = 0;
 	while (scale <= ROUGHCAST_LIMIT)
 	{
-		t += fabs(compute_3dperlin_factor(inter_point, scale, permutation, gradient) / scale);
-	   	scale *= 2;
+		t += fabs(compute_3dperlin_factor(inter_point, scale,
+			permutation, gradient) / scale);
+	  scale *= 2;
 	}
 	return t;
 }
 
-double	compute_marble_factor(t_3vecf inter_point, double scale, const unsigned char permutation[512], const t_3vecf gradient[16])
+double	compute_marble_factor(t_3vecf inter_point, double scale,
+	const unsigned char permutation[512], const t_3vecf gradient[16])
 {
-	double t = (0.5 + 0.5 * sin(scale * 2 * M_PI * (inter_point.val[0] + 2 * marble_noise(inter_point, scale, permutation, gradient))));
+	double t = (0.5 + 0.5 * sin(scale * 2 * M_PI * (inter_point.val[0] + 2
+		* marble_noise(inter_point, scale, permutation, gradient))));
 	return (t * t - .5);
 }
 
@@ -95,43 +85,37 @@ t_4vecf	get_wood_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 	t_text_proc	*text;
 
 	text = (t_text_proc *)obj->text.text_param;
-	wood_f = compute_wood_factor(inter_point, obj->text.scale.val[0], obj->data->permutation, obj->data->gradient);
-//	color.val[0] = obj->text.color[0].val[0] * (1 - marble_f) + obj->text.color[1].val[0] * marble_f;
-//	color.val[1] = obj->text.color[0].val[1] * (1 - marble_f) + obj->text.color[1].val[1] * marble_f;
-//	color.val[2] = obj->text.color[0].val[2] * (1 - marble_f) + obj->text.color[1].val[2] * marble_f;
-
-	color.val[0] = linear_interpolate(text->color[0].val[0], text->color[1].val[0], wood_f);
-	color.val[1] = linear_interpolate(text->color[0].val[1], text->color[1].val[1], wood_f);
-	color.val[2] = linear_interpolate(text->color[0].val[2], text->color[1].val[2], wood_f);
-	color.val[3] = linear_interpolate(text->color[0].val[3], text->color[1].val[3], wood_f);
+	wood_f = compute_wood_factor(inter_point, obj->text.scale.val[0],
+		obj->data->permutation, obj->data->gradient);
+	color.val[0] = linear_interpolate(text->color[0].val[0],
+		text->color[1].val[0], wood_f);
+	color.val[1] = linear_interpolate(text->color[0].val[1],
+		text->color[1].val[1], wood_f);
+	color.val[2] = linear_interpolate(text->color[0].val[2],
+		text->color[1].val[2], wood_f);
+	color.val[3] = linear_interpolate(text->color[0].val[3],
+		text->color[1].val[3], wood_f);
 	return (color);
 	(void)normal_inter;
 }
 
 t_4vecf	get_marble_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 {
-//	t_2vecf	text_coord;
-//	double	perlin_f;
 	double	marble_f;
 	t_4vecf	color;
 	t_text_proc	*text;
 
 	text = (t_text_proc *)obj->text.text_param;
-	//perlin_f = compute_perlin_factor(inter_point);
-//	perlin_f = compute_3dperlin_factor(assign_3vecf(inter_point.val[0] * 5, inter_point.val[1] * 5, inter_point.val[2] * 5), obj->text.scale.val[0]);
-//	text_coord = obj->get_text_coordinate(inter_point, normal_inter, obj);
-	marble_f = compute_marble_factor(inter_point, obj->text.scale.val[0], obj->data->permutation, obj->data->gradient);
-	//(1. + sin((text_coord.val[1] + perlin_f / 2) * 3.)) / 2.;
-	//wood_f = inter_point.val[0] * inter_point.val[0] + inter_point.val[1] * inter_point.val[1] + perlin_f;
-//	text_coord = obj->get_text_coordinate(inter_point, normal_inter, obj);
-//	color.val[0] = obj->text.color[0].val[0] * (1 - marble_f) + obj->text.color[1].val[0] * marble_f;
-//	color.val[1] = obj->text.color[0].val[1] * (1 - marble_f) + obj->text.color[1].val[1] * marble_f;
-//	color.val[2] = obj->text.color[0].val[2] * (1 - marble_f) + obj->text.color[1].val[2] * marble_f;
-
-	color.val[0] = linear_interpolate(text->color[0].val[0], text->color[1].val[0], marble_f);
-	color.val[1] = linear_interpolate(text->color[0].val[1], text->color[1].val[1], marble_f);
-	color.val[2] = linear_interpolate(text->color[0].val[2], text->color[1].val[2], marble_f);
-	color.val[3] = linear_interpolate(text->color[0].val[3], text->color[1].val[3], marble_f);
+	marble_f = compute_marble_factor(inter_point, obj->text.scale.val[0],
+		obj->data->permutation, obj->data->gradient);
+	color.val[0] = linear_interpolate(text->color[0].val[0],
+		text->color[1].val[0], marble_f);
+	color.val[1] = linear_interpolate(text->color[0].val[1],
+		text->color[1].val[1], marble_f);
+	color.val[2] = linear_interpolate(text->color[0].val[2],
+		text->color[1].val[2], marble_f);
+	color.val[3] = linear_interpolate(text->color[0].val[3],
+		text->color[1].val[3], marble_f);
 	return (color);
 	(void)normal_inter;
 }
@@ -143,55 +127,55 @@ t_4vecf	get_fbm_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 	t_text_proc	*text;
 
 	text = (t_text_proc *)obj->text.text_param;
-	//perlin_f = compute_perlin_factor(inter_point);
-//	perlin_f = compute_3dperlin_factor(assign_3vecf(inter_point.val[0] * 5, inter_point.val[1] * 5, inter_point.val[2] * 5), obj->text.scale.val[0]);
-//	text_coord = obj->get_text_coordinate(inter_point, normal_inter, obj);
 	fbm_f = compute_3dfbm_factor(inter_point, obj->text.scale.val[0]);
-	//(1. + sin((text_coord.val[1] + perlin_f / 2) * 3.)) / 2.;
-	//wood_f = inter_point.val[0] * inter_point.val[0] + inter_point.val[1] * inter_point.val[1] + perlin_f;
-//	text_coord = obj->get_text_coordinate(inter_point, normal_inter, obj);
-//	color.val[0] = obj->text.color[0].val[0] * (1 - fbm_f) + obj->text.color[1].val[0] * fbm_f;
-//	color.val[1] = obj->text.color[0].val[1] * (1 - fbm_f) + obj->text.color[1].val[1] * fbm_f;
-//	color.val[2] = obj->text.color[0].val[2] * (1 - fbm_f) + obj->text.color[1].val[2] * fbm_f;
-
-	color.val[0] = linear_interpolate(text->color[0].val[0], text->color[1].val[0], fbm_f);
-	color.val[1] = linear_interpolate(text->color[0].val[1], text->color[1].val[1], fbm_f);
-	color.val[2] = linear_interpolate(text->color[0].val[2], text->color[1].val[2], fbm_f);
-	color.val[3] = linear_interpolate(text->color[0].val[3], text->color[1].val[3], fbm_f);
+	color.val[0] = linear_interpolate(text->color[0].val[0],
+		text->color[1].val[0], fbm_f);
+	color.val[1] = linear_interpolate(text->color[0].val[1],
+		text->color[1].val[1], fbm_f);
+	color.val[2] = linear_interpolate(text->color[0].val[2],
+		text->color[1].val[2], fbm_f);
+	color.val[3] = linear_interpolate(text->color[0].val[3],
+		text->color[1].val[3], fbm_f);
 	return (color);
 	(void)normal_inter;
 }
 
-t_4vecf	get_image_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
+int     get_pixel_addr(t_2vecf text_coord, t_text_img *text, t_obj *obj)
 {
-	t_2vecf	text_coord;
-	t_4vecf	color;
-	t_text_img	*text;
 	int		row;
 	int		col;
-//	int		offset_row;
-//	int		offset_col;
-	int		pixel_addr;
 
-//	printf("halloooo\n");
+	if (text_coord.val[1] < 0)
+		col = (int)((1 - ((-text_coord.val[1] - (int)-text_coord.val[1])))
+			* (double)(text->width));
+	else
+		col = (int)((text_coord.val[1] - (int)text_coord.val[1])
+			* (double)(text->width));
+	if (text_coord.val[0] < 0)
+		row = (int)((1 - (-text_coord.val[0] - (int)-text_coord.val[0]))
+			* (double)(text->height));
+	else
+		row = (int)((text_coord.val[0] - (int)text_coord.val[0])
+			* (double)(text->height));
+	row += obj->text.offset.val[0] * (double)(text->height);
+	col += (1 - obj->text.offset.val[1]) * (double)(text->width);
+	row %= text->height;
+	col %= text->width;
+	return (row * text->width + col);
+}
+
+t_4vecf	get_image_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
+{
+	t_2vecf			text_coord;
+	t_4vecf			color;
+	t_text_img	*text;
+	int					pixel_addr;
+
 	text = (t_text_img *)obj->text.text_param;
 	text_coord = obj->get_text_coordinate(inter_point, normal_inter, obj);
 	text_coord.val[1] *= obj->text.scale.val[1];
 	text_coord.val[0] *= obj->text.scale.val[0];
-	if (text_coord.val[1] < 0)
-		col = (int)((1 - ((-text_coord.val[1] - (int)-text_coord.val[1]))) * (double)(text->width));
-	else
-		col = (int)((text_coord.val[1] - (int)text_coord.val[1]) * (double)(text->width));
-	if (text_coord.val[0] < 0)
-		row = (int)((1 - (-text_coord.val[0] - (int)-text_coord.val[0])) * (double)(text->height));
-	else
-		row = (int)((text_coord.val[0] - (int)text_coord.val[0]) * (double)(text->height));
-	row += obj->text.offset.val[0] * (double)(text->height);
-	col += (1 - obj->text.offset.val[1]) * (double)(text->width);
-
-	row %= text->height;
-	col %= text->width;
-	pixel_addr = row * text->width + col;
+	pixel_addr = get_pixel_addr(text_coord, text, obj);
 	color.val[0] = (double)(text->pixels[pixel_addr] >> 24 & 0xff) / 255.;
 	color.val[1] = (double)(text->pixels[pixel_addr] >> 16 & 0xff) / 255.;
 	color.val[2] = (double)(text->pixels[pixel_addr] >> 8 & 0xff) / 255.;
@@ -204,16 +188,17 @@ t_4vecf	get_grid_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 {
 	t_2vecf	text_coord;
 	t_text_proc	*text;
+	int		u_grid;
+	int		v_grid;
 
 	text = (t_text_proc *)obj->text.text_param;
 	text_coord = obj->get_text_coordinate(inter_point, normal_inter, obj);
 	text_coord.val[0] *= obj->text.scale.val[0];
 	text_coord.val[1] *= obj->text.scale.val[1];
-	text_coord.val[0] -= text_coord.val[0] < 0 ? (1 + fmod(text_coord.val[0], 1)) : fmod(text_coord.val[0], 1);
-	text_coord.val[1] -= text_coord.val[1] < 0 ? (1 + fmod(text_coord.val[1], 1)) : fmod(text_coord.val[1], 1);
-	int		u_grid;
-	int		v_grid;
-
+	text_coord.val[0] -= text_coord.val[0] < 0 ? (1 + fmod(text_coord.val[0], 1))
+		: fmod(text_coord.val[0], 1);
+	text_coord.val[1] -= text_coord.val[1] < 0 ? (1 + fmod(text_coord.val[1], 1))
+		: fmod(text_coord.val[1], 1);
 	u_grid = (fmod(text_coord.val[0], 2) == 0);
 	v_grid = (fmod(text_coord.val[1], 2) == 0);
 	if ((u_grid ^ v_grid))
@@ -221,7 +206,8 @@ t_4vecf	get_grid_color(t_3vecf inter_point, t_3vecf normal_inter, t_obj *obj)
 	return (text->color[1]);
 }
 
-t_4vecf			(*assign_text_color_function(t_text_type type))(t_3vecf, t_3vecf, struct s_obj *)
+t_4vecf			(*assign_text_color_function(t_text_type type))(t_3vecf,
+	t_3vecf, struct s_obj *)
 {
 		if (type == TEXT_PERLIN)
 			return (&get_perlin_color);
@@ -243,8 +229,10 @@ t_text	generate_random_texture(t_obj *obj)
 	t_text		text;
 	t_text_proc	*param;
 
-	text.scale.val[0] = get_random_number(time(NULL) << 6) * get_random_number(time(NULL) >> 4) * 10;
-	text.scale.val[1] = get_random_number(time(NULL) >> 6) * get_random_number(time(NULL) << 3) * 10;
+	text.scale.val[0] = get_random_number(time(NULL) << 6)
+		* get_random_number(time(NULL) >> 4) * 10;
+	text.scale.val[1] = get_random_number(time(NULL) >> 6)
+		* get_random_number(time(NULL) << 3) * 10;
 	if (!(param = ft_memalloc(sizeof(t_text_proc))))
 		exit(EXIT_FAILURE);
 	param->color[0] = generate_random_color(time(NULL), 0.25);
--- srcs/triangle.c
+++ srcs/triangle.c
@@ -18,41 +18,9 @@ int		check_inside_triangle(t_3vecf point, t_obj *triangle)
 	(void)point;
 	(void)triangle;
 }
-/*
-void	generate_new_cylinder(t_data *data)
-{
-	t_obj		*cylinder;
-	t_cylinder		*param;
-	t_3vecf		dir;
 
-	dir = mult_3vecf_33matf(mult_3vecf_33matf(window_to_view(0, 0, data->size.val[0], data->size.val[1]), data->rot_mat[1]), data->rot_mat[0]);
-	normalize_3vecf(&dir);
-	if (!(cylinder = ft_memalloc(sizeof(t_obj))))
-		return ;
-	if (!(param = ft_memalloc(sizeof(t_cylinder))))
-		return ;
-	param->radius = get_random_number(time(NULL)) * 1.5;
-	param->tip.val[0] = data->camera->origin.val[0] + dir.val[0] * 2;
-	param->tip.val[1] = data->camera->origin.val[1] + dir.val[1] * 2;
-	param->tip.val[2] = data->camera->origin.val[2] + dir.val[2] * 2;
-	param->center = add_3vecf(assign_3vecf(0, 1, 0), param->tip);
-	cylinder->obj_param = param;
-	cylinder->obj_type = OBJ_CYLINDER;
-	cylinder->check_inside = &check_inside_cylinder;
-	cylinder->ray_intersect = &ray_intersect_cylinder;
-	cylinder->get_normal_inter = &get_normal_intersect_cylinder;
-	cylinder->get_origin = &get_origin_cylinder;
-	cylinder->move = &move_cylinder;
-	cylinder->rotate = &rotate_cylinder;
-	cylinder->get_text_coordinate = &get_text_coordinate_cylinder;
-	cylinder->get_text_color = &get_uni_color;
-	cylinder->text = generate_random_texture();
-	set_bump_own(cylinder);
-	add_object(cylinder, data);
-	data->new_obj = 1;
-}
-*/
-t_2vecf	get_text_coordinate_triangle(t_3vecf inter_point, t_3vecf normal_inter, t_obj *triangle)
+t_2vecf	get_text_coordinate_triangle(t_3vecf inter_point,
+	t_3vecf normal_inter, t_obj *triangle)
 {
 	t_2vecf	text_coord;
 
@@ -70,19 +38,10 @@ void	move_triangle(t_obj *triangle, t_3vecf dir, double fact)
 	t_cut	*cuts;
 
 	param = (t_triangle *)triangle->obj_param;
-	param->a.val[0] += dir.val[0] * fact;
-	param->a.val[1] += dir.val[1] * fact;
-	param->a.val[2] += dir.val[2] * fact;
-	param->b.val[0] += dir.val[0] * fact;
-	param->b.val[1] += dir.val[1] * fact;
-	param->b.val[2] += dir.val[2] * fact;
-	param->c.val[0] += dir.val[0] * fact;
-	param->c.val[1] += dir.val[1] * fact;
-	param->c.val[2] += dir.val[2] * fact;
-	param->origin.val[0] += dir.val[0] * fact;
-	param->origin.val[1] += dir.val[1] * fact;
-	param->origin.val[2] += dir.val[2] * fact;
-
+	param->a = add_3vecf(param->a, product_c3vecf(dir, fact));
+	param->b = add_3vecf(param->b, product_c3vecf(dir, fact));
+	param->c = add_3vecf(param->c, product_c3vecf(dir, fact));
+	param->origin = add_3vecf(param->origin, product_c3vecf(dir, fact));
 	cuts = triangle->cuts;
 	while (cuts)
 	{
@@ -101,15 +60,12 @@ void	rotate_triangle(t_obj *triangle, t_3vecf orig, t_33matf rot_mat[2])
 	param->a = sub_3vecf(param->a, orig);
 	param->b = sub_3vecf(param->b, orig);
 	param->c = sub_3vecf(param->c, orig);
-
 	param->a = mult_3vecf_33matf(param->a, rot_mat[1]);
 	param->b = mult_3vecf_33matf(param->b, rot_mat[1]);
 	param->c = mult_3vecf_33matf(param->c, rot_mat[1]);
-
 	param->a = mult_3vecf_33matf(param->a, rot_mat[0]);
 	param->b = mult_3vecf_33matf(param->b, rot_mat[0]);
 	param->c = mult_3vecf_33matf(param->c, rot_mat[0]);
-
 	param->a = add_3vecf(orig, param->a);
 	param->b = add_3vecf(orig, param->b);
 	param->c = add_3vecf(orig, param->c);
@@ -122,73 +78,87 @@ void	rotate_triangle(t_obj *triangle, t_3vecf orig, t_33matf rot_mat[2])
 	}
 }
 
-t_3vecf	get_origin_triangle(t_obj *triangle)
+t_3vecf	get_origin_triangle(t_obj *triangle) // a tej
 {
-	// t_triangle *param;
-	// t_3vecf			origin;
-	//
-	// param = (t_triangle *)triangle->obj_param;
-	// origin.val[0] = (param->a.val[0] + param->b.val[0] + param->c.val[0]) / 3;
-	// origin.val[1] = (param->a.val[1] + param->b.val[1] + param->c.val[1]) / 3;
-	// origin.val[2] = (param->a.val[2] + param->b.val[2] + param->c.val[2]) / 3;
-	// return (origin);
 	return (((t_triangle *)triangle->obj_param)->origin);
 }
 
-t_3vecf	get_normal_intersect_triangle(t_3vecf inter_point, t_obj *triangle, int sp_id)
+t_3vecf get_triangle_origin(t_obj *triangle,
+	t_triangle *triangle_param, int sp_id)
+{
+	if (sp_id)
+		return (move_3vecf(triangle_param->origin, triangle->motions, sp_id));
+	return (triangle_param->origin);
+}
+t_3vecf	get_normal_intersect_triangle(t_3vecf inter_point,
+	t_obj *triangle, int sp_id)
 {
 	t_triangle	*param;
 	t_3vecf		normal;
 
 	param = (t_triangle *)triangle->obj_param;
-	normal = product_3vecf(sub_3vecf(param->b, param->a), sub_3vecf(param->c, param->a));
+	normal = product_3vecf(sub_3vecf(param->b, param->a),
+		sub_3vecf(param->c, param->a));
 	normalize_3vecf(&normal);
 	return (normal);
 	(void)inter_point;
 	(void)sp_id;
 }
 
-int	ray_intersect_triangle(t_leq l, t_obj *triangle, t_dist dist, int sp_id)
+void  get_triangle_corner(t_3vecf corner[3], t_obj *triangle, int sp_id)
 {
-	t_3vecf	ab;
-	t_3vecf	ac;
-	t_3vecf	dir_ac;
-	t_3vecf	t_vec;
-	t_3vecf	q_vec;
-	double delta;
-	double inv_delta;
-	double u, v;
-	double tdist;
-	t_triangle	*param;
-
-	t_3vecf	a, b, c;
+	t_triangle *param;
 
 	param = (t_triangle *)triangle->obj_param;
-	a = sp_id ? move_3vecf(param->a, triangle->motions, sp_id) : param->a;
-	b = sp_id ? move_3vecf(param->b, triangle->motions, sp_id) : param->b;
-	c = sp_id ? move_3vecf(param->c, triangle->motions, sp_id) : param->c;
-	ab = sub_3vecf(b, a);
-	ac = sub_3vecf(c, a);
-	dir_ac = product_3vecf(l.dir, ac);
-	delta = dot_product_3vecf(ab, dir_ac);
+	corner[0] = sp_id ? move_3vecf(param->a, triangle->motions, sp_id) : param->a;
+	corner[1] = sp_id ? move_3vecf(param->b, triangle->motions, sp_id) : param->b;
+	corner[2] = sp_id ? move_3vecf(param->c, triangle->motions, sp_id) : param->c;
+}
+
+double get_inverse_delta(t_3vecf a_vec, t_3vecf dir_ac)
+{
+	double delta;
+
+	delta = dot_product_3vecf(a_vec, dir_ac);
 	if (is_null(delta))
 		return (0);
-	inv_delta = 1. / delta;
-	t_vec = sub_3vecf(l.orig, a);
-	u = dot_product_3vecf(t_vec, dir_ac) * inv_delta;
-	if (u < 0 || u > 1)
-		return (0);
-	q_vec = product_3vecf(t_vec, ab);
-	v = dot_product_3vecf(l.dir, q_vec) * inv_delta;
+	return (1. / delta);
+}
+
+int check_triangle_dist(double u, double v, t_dist dist, double root)
+{
 	if (v < 0 || u + v > 1)
 		return (0);
-	tdist = dot_product_3vecf(ac, q_vec) * inv_delta;
-	if (tdist < *(dist.dist) && tdist > dist.min_dist && tdist < dist.max_dist)
+	if (root < *(dist.dist) && root > dist.min_dist && root < dist.max_dist)
 	{
-		*(dist.dist) = tdist;
+		*(dist.dist) = root;
 		return (1);
 	}
 	return (0);
+}
+
+int	ray_intersect_triangle(t_leq l, t_obj *triangle, t_dist dist, int sp_id)
+{
+	t_3vecf	a_vec[2];
+	t_3vecf corner[3];
+	t_3vecf	dir_ac;
+	t_3vecf tq_vec[2];
+	double  tmp[3];
+
+	get_triangle_corner(corner, triangle, sp_id);
+	a_vec[0] = sub_3vecf(corner[1], corner[0]);
+	a_vec[1] = sub_3vecf(corner[2], corner[0]);
+	dir_ac = product_3vecf(l.dir, a_vec[1]);
+	if (is_null((tmp[0] = get_inverse_delta(a_vec[0], dir_ac))))
+		return (0);
+	tq_vec[0] = sub_3vecf(l.orig, corner[0]);
+	tmp[1] = dot_product_3vecf(tq_vec[0], dir_ac) * tmp[0];
+	if (tmp[1] < 0 || tmp[1] > 1)
+		return (0);
+	tq_vec[1] = product_3vecf(tq_vec[0], a_vec[0]);
+	tmp[2] = dot_product_3vecf(l.dir, tq_vec[1]) * tmp[0];
+	return (check_triangle_dist(tmp[1], tmp[2], dist,
+		dot_product_3vecf(a_vec[1], tq_vec[1]) * tmp[0]));
 }
 
 void  assign_triangle_function(t_obj *triangle)
@@ -203,103 +173,52 @@ void  assign_triangle_function(t_obj *triangle)
 	triangle->get_text_coordinate = &get_text_coordinate_triangle;
 }
 
+void  get_random_corner(t_triangle *param)
+{
+	t_3vecf		dir;
+
+	dir = assign_3vecf(
+		get_random_number((time(NULL) * 0xcacacaca) << 16) - 0.5,
+		get_random_number((time(NULL) * 0xfeacdef) << 8) - 0.5,
+		get_random_number((time(NULL) * 0x1056ffe) << 12) - 0.5);
+	normalize_3vecf(&dir);
+	param->a = add_3vecf(dir, param->origin);
+	dir = assign_3vecf(
+		get_random_number((time(NULL) * 0xcacacaca) << 13) - 0.5,
+		get_random_number((time(NULL) * 0xfeacdef) << 9) - 0.5,
+		get_random_number((time(NULL) * 0x1056ffe) << 3) - 0.5);
+	normalize_3vecf(&dir);
+	param->b = add_3vecf(dir, param->origin);
+	dir = assign_3vecf(
+		get_random_number((time(NULL) * 0xcacacaca) << 12) - 0.5,
+		get_random_number((time(NULL) * 0xfeacdef) << 6) - 0.5,
+		get_random_number((time(NULL) * 0x1056ffe) << 0) - 0.5);
+	normalize_3vecf(&dir);
+	param->c = add_3vecf(dir, param->origin);
+}
+
 void	generate_new_triangle(t_data *data)
 {
 	t_obj		*triangle;
 	t_triangle	*param;
 	t_3vecf		dir;
 
-	dir = mult_3vecf_33matf(mult_3vecf_33matf(window_to_view(0, 0, data->size.val[0], data->size.val[1]), data->rot_mat[1]), data->rot_mat[0]);
+	dir = mult_3vecf_33matf(mult_3vecf_33matf(window_to_view(0, 0,
+		data->size.val[0], data->size.val[1]), data->rot_mat[1]), data->rot_mat[0]);
 	normalize_3vecf(&dir);
 	if (!(triangle = ft_memalloc(sizeof(t_obj))))
 		return ;
 	if (!(param = ft_memalloc(sizeof(t_triangle))))
 		return ;
-//	param->radius = get_random_number(time(NULL)) * 2.5;
-	param->origin.val[0] = data->camera->origin.val[0] + dir.val[0] * 2;
-	param->origin.val[1] = data->camera->origin.val[1] + dir.val[1] * 2;
-	param->origin.val[2] = data->camera->origin.val[2] + dir.val[2] * 2;
-	dir = assign_3vecf(get_random_number((time(NULL) * 0xcacacaca) << 16) - 0.5, get_random_number((time(NULL) * 0xfeabcdef) << 8) - 0.5, get_random_number((time(NULL) * 0x1056ffe) << 12) - 0.5);
-	normalize_3vecf(&dir);
-	param->a = add_3vecf(dir, param->origin);
-	dir = assign_3vecf(get_random_number((time(NULL) * 0xcacacaca) << 13) - 0.5, get_random_number((time(NULL) * 0xfeabcdef) << 9) - 0.5, get_random_number((time(NULL) * 0x1056ffe) << 3) - 0.5);
-	normalize_3vecf(&dir);
-	param->b = add_3vecf(dir, param->origin);
-	dir = assign_3vecf(get_random_number((time(NULL) * 0xcacacaca) << 12) - 0.5, get_random_number((time(NULL) * 0xfeabcdef) << 6) - 0.5, get_random_number((time(NULL) * 0x1056ffe) << 0) - 0.5);
-	normalize_3vecf(&dir);
-	param->c = add_3vecf(dir, param->origin);
+	param->origin = add_3vecf(data->camera->origin, product_c3vecf(dir, 2));
+	get_random_corner(param);
 	param->origin.val[0] = (param->a.val[0] + param->b.val[0] + param->c.val[0]) / 3.;
 	param->origin.val[1] = (param->a.val[1] + param->b.val[1] + param->c.val[1]) / 3;
 	param->origin.val[2] = (param->a.val[2] + param->b.val[2] + param->c.val[2]) / 3;
-	// param->origin.val[1] = data->camera->origin.val[1] + dir.val[1] * 2;
-	// param->origin.val[2] = data->camera->origin.val[2] + dir.val[2] * 2;
-
 	triangle->obj_param = param;
 	assign_triangle_function(triangle);
 	triangle->text = generate_random_texture(triangle);
-	//triangle->get_bump_mapping = NULL;
 	set_bump_own(triangle);
-	//texture needed
 	add_object(triangle, data);
 	data->new_obj = 1;
 }
-/*int		parse_triangle(char *line, t_data *data)
-  {
-  int			i;
-  t_obj		*triangle;
-  t_triangle	*triangle_param;
-
-  if (!(triangle = malloc(sizeof(t_obj))) || !(triangle_param = malloc(sizeof(t_triangle))))
-  return (0);
-  i = 6;
-  while (ft_isspace(line[i]))
-  ++i;
-  if (line[i] != '(' || (i = parse_3vecf(line, i, &triangle_param->origin)) == -1)
-  {
-  ft_printf("Syntax error: triangle syntax: triangle(origin)(radius)(color)(reflection)(refraction)\n");
-  return (0);
-  }
-  while (ft_isspace(line[i]))
-  ++i;
-  if (line[i] != '(' || (i = parse_double(line, i, &triangle_param->radius)) == -1)
-  {
-  ft_printf("Syntax error: triangle syntax: triangle(origin)(radius)(color)(reflection)(refraction)\n");
-  return (0);
-  }
-  while (ft_isspace(line[i]))
-  ++i;
-  if (line[i] != '(' || (i = parse_texture(line, i, triangle)) == -1)
-  {
-  ft_printf("Syntax error: triangle syntax: triangle(origin)(radius)(color)(reflection)(refraction)\n");
-  return (0);
-  }
-  while (ft_isspace(line[i]))
-  ++i;
-  if (line[i] != '(' || (i = parse_double(line, i, &triangle->reflection)) == -1)
-  {
-  ft_printf("Syntax error: triangle syntax: triangle(origin)(radius)(color)(reflection)(refraction)\n");
-  return (0);
-  }
-  while (ft_isspace(line[i]))
-  ++i;
-  if (line[i] != '(' || (i = parse_double(line, i, &triangle->refraction)) == -1)
-  {
-  ft_printf("Syntax error: triangle syntax: triangle(origin)(radius)(color)(reflection)(refraction)\n");
-  return (0);
-  }
-
-//printf("triangle : %f %f %f && %f && %f %f %f\n", triangle_param->origin.val[0], triangle_param->origin.val[1], triangle_param->origin.val[2], triangle_param->radius, triangle->color.val[0], triangle->color.val[1], triangle->color.val[2]);
-triangle->obj_param = triangle_param;
-triangle->obj_type = OBJ_triangle;
-triangle->ray_intersect = &ray_intersect_triangle;
-triangle->get_normal_inter = &get_normal_intersect_triangle;
-triangle->get_text_coordinate = &get_text_coordinate_triangle;
-if (data->objs)
-{
-triangle->next = data->objs;
-}
-else
-triangle->next = NULL;
-data->objs = triangle;
-return (1);
-}*/
